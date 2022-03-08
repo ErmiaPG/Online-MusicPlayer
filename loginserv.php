6968d8ccebd3fc1d1b77beb8fce8f8c4408e7fd4
ErmiaPG <80034976+ErmiaPG@users.noreply.github.com>
2022-03-08 23:34:07 +0330

    Update loginserv.php

--- loginserv.php
+++ loginserv.php
@@ -1,28 +1,28 @@
 <?php
     ini_set('display_errors', 1);
+    session_start();
     $error='';
-    if(isset($_POST['submit'])){
-        if(empty($_POST['user']) || empty($_POST['pass'])){
-            $error = "Username or Password is Invalid";
-        } 
-        else
-        {
-            $user=$_POST['user'];
-            $pass=$_POST['pass'];
-            $conn = mysqli_connect("sql213.gigfa.com", "gigfa_30646934", "gnzq65fs", "gigfa_30646934_Online_Musicplayer");
-            $db = mysqli_select_db($conn, "userpass");
-            $query = mysqli_query($conn, "SELECT * FROM userpass WHERE pass='$pass' AND user='$user'");
+    $user = $pass = "";
+    if (isset($_POST['user']) and isset($_POST['pass'])){
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+    }
+    $con = mysqli_connect("sql213.gigfa.com", "gigfa_30646934", "gnzq65fs", "gigfa_30646934_Online_Musicplayer");
+    mysqli_select_db($con, "userpass");
+    $s = "SELECT * FROM userpass WHERE pass='$pass' AND user='$user'";
 
-            $rows = mysqli_num_rows($query);
-            if($rows == 1){
-                header("Location: index.html");
-            }
-            else
-            {
-                $error = "Username of Password is Invalid";
-            }
-        mysqli_close($conn);
-        }
+    $result = mysqli_query($con, $s);
+
+    $num = mysqli_num_rows($result);
+
+   if($num == 1 and isset($_POST['submit-login'])){
+        $_SESSION['Loged in'] = true;
+        header('location: http://onlinemusicplayerproject.gigfa.com');
+    }  
+    else if($num != 1 and isset($_POST['submit-login']))
+    {
+        $error = "Username of Password is Invalid";
     }
 
+    
 ?>
