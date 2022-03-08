41aee5aeb2a1d698f358b8c139cf2263ecf61746
ErmiaPG <80034976+ErmiaPG@users.noreply.github.com>
2022-03-08 23:29:32 +0330

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,17 +1,31 @@
 <?php
-ini_set('display_errors', 1);
-include("loginserv.php");
+    ini_set('display_errors', 1);
+    include("../loginserv.php");
+    session_start();
+    $Loged_in = false;
+    if (isset($_SESSION['Loged in']))
+    {
+        if($_SESSION['Loged in'] == true)
+           {$Loged_in = true;}
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Register</title>
+    <title>Login</title>
+    <link rel="shortcut icon" href="../wp-content/uploads/favicon.ico">
+    <link rel="icon" type="image/png" href="../wp-content/uploads/favicon-192x192.png" sizes="192x192">
+    <link rel="apple-touch-icon" sizes="180x180" href="../wp-content/uploads/apple-touch-icon-180x180.png">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
 
     <style>
         body {
@@ -67,7 +81,10 @@ include("loginserv.php");
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Online MusicPlayer</a>
+        <a class="navbar-brand" href="#">
+        <img src="../wp-content/uploads/favicon-192x192.png" width="30" height="30"  class="d-inline-block align-top" alt="">
+        Online MusicPlayer
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -75,29 +92,35 @@ include("loginserv.php");
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="Login.php">Login / Sign up</a>
+                    <?php 
+                    if($Loged_in == false)
+                    { echo '<a class="nav-link" href="../Login">Login / Sign up</a>';}
+                    else
+                    { echo '<a class="nav-link" href="../Profile">Profile</a>';}
+                    ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="Playlist.html">Playlist</a>
+                        <a class="nav-link" href="../Playlist">Playlist</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="Musics.html">Musics</a>
+                        <a class="nav-link" href="../Musics">Musics</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">About us</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="/" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contact Us</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown01">
-                            <a class="dropdown-item" href="/">Website</a>
-                            <a class="dropdown-item" href="https://www.instagram.com/ermiapg" target="_blank">Instagram</a>
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Social Media</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="http://onlinemusicplayerproject.gigfa.com">Website</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="https://www.instagram.com/ermiapg" target="_blank">Instgram</a>
                             <a class="dropdown-item" href="https://www.youtube.com/channel/UCBZ8j63pCuhI27nJOXg3s2g?view_as=subscriber" target="_blank">You Tube</a>
                         </div>
                     </li>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <form class="form-inline">
+                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
@@ -117,9 +140,9 @@ include("loginserv.php");
                 </div>
             </div> 
             <input class="btn btn-success" type="submit" value="Login" name="submit-login">
-            <span><?php echo $error; ?></span>
+            <span style="color:red; font-size: 11px"><?php echo $error; ?></span>
             <br>
-            <a style="color: gray" font-size="12">Don't have an account?</a> <a href="SignUp.php" class="ml-2">Sign Up</a>
+            <a style="color: gray" font-size="12">Don't have an account?</a> <a href="../SignUp" class="ml-2">Sign Up</a>
         </form>
     </div>
 
